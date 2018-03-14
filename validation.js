ba55863aa48dd54ecadbdc3cbe69921b9c7c428d
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-03-14 09:40:02 +0530

    final changes with styles

--- validation.js
+++ validation.js
@@ -4,7 +4,8 @@ const patterns = {
     telephone: /^\d{11}$/,
     username: /^[a-z\d]{5,12}$/i,
     password: /^[w@-]{8,20}$/,
-    slug: /^[a-z\d-]{8,20}$/
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 };
 
 // grab all the inputs
